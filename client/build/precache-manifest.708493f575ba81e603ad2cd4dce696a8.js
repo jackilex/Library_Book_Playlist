48bb888a06c7cdf39e17e0b5993d9d41a1691cb8
Alex Saint Victor <jackilex91@gmail.com>
2020-09-24 00:09:23 -0500

    test

---
+++ client/build/precache-manifest.708493f575ba81e603ad2cd4dce696a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e401aa709f44ee8710d0edc154afe2ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c4643617e6a10680e6b",
+    "url": "/static/css/2.bb93bc02.chunk.css"
+  },
+  {
+    "revision": "b48c8fe025bf59c11176",
+    "url": "/static/css/main.3077e57e.chunk.css"
+  },
+  {
+    "revision": "4c4643617e6a10680e6b",
+    "url": "/static/js/2.b2717ffb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b2717ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48c8fe025bf59c11176",
+    "url": "/static/js/main.87179612.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
